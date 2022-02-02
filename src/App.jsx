7dfa0f49464194aaa819d5c24f099fa5617e00f5
Jules <jules1979ve@gmail.com>
2022-02-02 09:08:40 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import './plugings/sweetalert2.css'
+import Navbar from './components/Navbar/jsx/Navbar'
+
+
+function App() {
+  return (
+    <div className="App">
+     < Navbar />
+    </div>
+  )
+}
+
+export default App
