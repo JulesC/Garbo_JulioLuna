f64fa74ede00d2bf720b43e8cb64e3e90d893c79
Jules <jules1979ve@gmail.com>
2022-02-04 16:12:28 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import './plugings/sweetalert2.css'
 import Navbar from './components/Navbar/jsx/Navbar'
-
+import ItemListContainer from './components/Navbar/jsx/ItemListContainer'
 
 function App() {
+  const Subtitulo = 'Since @2019 - Todos los derechos reservados.'
+
   return (
     <div className="App">
-     < Navbar />
+      < Navbar />
+      <header className="App-header">
+        < ItemListContainer Greetings={Subtitulo} />
+      </header >
     </div>
   )
 }
---
+++ src/components/Navbar/jsx/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ItemListContainer = (props) => {
+    
+    return (
+        <>
+            <h1> GARBO. <h2 className="text"> {props.Greetings}</h2></h1>
+        </>
+    )
+}
+
+export default ItemListContainer
